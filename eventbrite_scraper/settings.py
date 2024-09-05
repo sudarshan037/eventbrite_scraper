d882afff812a502809789b79bdf5b526344d920b
Ubuntu <sudarshan@vm-develop.dl0s0evskv0edmwaxachlcwe1a.rx.internal.cloudapp.net>
2024-09-05 11:25:42 +0000

    increased download delay

--- eventbrite_scraper/settings.py
+++ eventbrite_scraper/settings.py
@@ -52,14 +52,14 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_START_DELAY = 5  # Initial download delay
 AUTOTHROTTLE_MAX_DELAY = 60   # Maximum download delay
 
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 2
-CONCURRENT_REQUESTS_PER_IP = 2
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 1
 
 DUPEFILTER_CLASS = 'scrapy.dupefilters.BaseDupeFilter'
 HTTPERROR_ALLOWED_CODES = [404, 429]
