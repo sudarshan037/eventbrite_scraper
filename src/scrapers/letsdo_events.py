af7eac124329fbbe13965e38e9d3593dc7944af9
Sudarshan Choudhary <sudarshan037@gmail.com>
2025-01-14 15:00:24 +0530

    added organiser name for lets do this

--- src/scrapers/letsdo_events.py
+++ src/scrapers/letsdo_events.py
@@ -9,8 +9,7 @@ async def process(record, page):
         record['event_name'] = await utils.get_text(page, '//h1')
         record['event_date'] = await utils.get_text(page,'//div[contains(text(), "View logistics")]/preceding-sibling::h6[contains(text(), ",")]')
         record['location'] = await utils.get_text(page,'//div[contains(text(), "Where and when")]/following-sibling::div//div[contains(text(), "Location")]/following-sibling::div')
-        record['organizer_name'] = await utils.get_text(page,'//div[contains(@id, "organiser") or contains(@class, "organiser")]//div/text()')
-
+        record['organizer_name'] = await utils.get_text(page,"//main//button[contains(text(), 'By')]")
     except Exception as e:
         print(f"Error Fetching details for url -> {record['url']}: {e}")
     return record
