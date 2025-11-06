dbc8c894f0a3cb296bab2368d13c4107a0df4d82
Sudarshan Choudhary <sudarshan037@gmail.com>
2025-11-06 10:49:40 +0530

    added city and state in eventbrite_events

--- src/scrapers/eventbrite_events.py
+++ src/scrapers/eventbrite_events.py
@@ -1,6 +1,68 @@
 from src import utils
 import aiohttp
 
+abbreviation_to_name = {
+    # https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States#States.
+    "AK": "Alaska",
+    "AL": "Alabama",
+    "AR": "Arkansas",
+    "AZ": "Arizona",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "IA": "Iowa",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "MA": "Massachusetts",
+    "MD": "Maryland",
+    "ME": "Maine",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MO": "Missouri",
+    "MS": "Mississippi",
+    "MT": "Montana",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "NE": "Nebraska",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NV": "Nevada",
+    "NY": "New York",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VA": "Virginia",
+    "VT": "Vermont",
+    "WA": "Washington",
+    "WI": "Wisconsin",
+    "WV": "West Virginia",
+    "WY": "Wyoming",
+    # https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States#Federal_district.
+    "DC": "District of Columbia",
+    # https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States#Inhabited_territories.
+    "AS": "American Samoa",
+    "GU": "Guam GU",
+    "MP": "Northern Mariana Islands",
+    "PR": "Puerto Rico PR",
+    "VI": "U.S. Virgin Islands",
+}
+
 async def fetch_event_details(organiser_id):
     url = f"https://www.eventbrite.com/api/v3/organizers/{organiser_id}/?expand.organizer=follow_status"
     headers = {
@@ -53,6 +115,10 @@ async def process(record, page):
         elements = await page.query_selector_all("//div[contains(@class, 'Location-module__addressWrapper')]/p")
         texts = [await el.inner_text() for el in elements]
         record['location'] = ", ".join(texts)
+
+        # City and State
+        record['city'] = texts[-1].split(',')[0].strip() if texts else ""
+        record["state"] = abbreviation_to_name.get(texts[-1].split(',')[1].strip()[:2] if texts and len(texts[-1].split(',')) > 1 else "", "")
 
         record['organiser_name'] = await utils.get_text(page, "//a[contains(@class, 'OrganizerLink-module__OrganizerLink')]")
         record['followers'] = follow_status.get("num_followers", 0)
